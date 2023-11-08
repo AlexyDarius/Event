e7820d528a9adfaa590e33d0f743ffba551a8633
Roman Alexy <alexy.roman@alumni.enac.fr>
2023-11-08 11:51:02 +0100

    event displayer

--- web_src/event.php
+++ web_src/event.php
@@ -2,7 +2,7 @@
 include $_SERVER['DOCUMENT_ROOT']. '/includes/head.php'
 ?>
 
-    <title>Darius Pizza - Galerie</title>
+    <title>DariusDev - Event</title>
     <link rel="stylesheet" type="text/css" href="/modules/event/css/style.css">
 </head>
 <body style="--bs-body-bg: #e3f3f5;--bs-body-bg-t: rgba(227, 243, 245, 0.9);--bs-primary: #0c3028;--bs-secondary: #f53219;--bs-primary-t: rgba(12, 48, 40, 0.85);background: var(--bs-body-bg);font-family: Lato-Regular;color: var(--bs-primary);">
@@ -20,7 +20,7 @@ include $_SERVER['DOCUMENT_ROOT']. '/includes/navbar.php'
     <div id="image-container">
 
 <?php
-// require $_SERVER['DOCUMENT_ROOT']. '/modules/event/requires/event_displayer.php';
+require $_SERVER['DOCUMENT_ROOT']. '/modules/event/requires/event_displayer.php';
 ?>
 
     </div>
---
+++ web_src/modules/event/requires/event_displayer.php
@@ -0,0 +1,57 @@
+<?php
+// Connect to the MySQL database (adjust the connection details as per your configuration)
+$conn = new mysqli("localhost", "root", "root", "dariusdev_db");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve image information from the database
+$sql = "SELECT title, date, place, img_filename, text, link FROM dariusdev_event ORDER BY date DESC";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $imagePath = "modules/event/images/" . $row['img_filename'];
+        $title = $row['title'];
+        $date = $row['date'];
+        $place = $row['place'];
+        $text = $row['text'];
+        $link = $row['link'];
+        
+        echo "<section style='margin-right: 32px;margin-left: 32px;'>";
+        echo "<div class='container'>";
+        echo "<div class='row d-flex justify-content-center'>";
+        echo " <div class='col-md-12'>";
+        echo "<div>";
+        echo "<h3 style='text-align: center;font-weight: bold;'>$title</h3>";
+        echo "<p class='text-center'>Location : $place</p>";
+        echo "</div>";
+        echo "</div>";
+        echo "</div>";
+        echo "<div class='row'>";
+        echo "<div class='col d-flex justify-content-center'>";
+        echo "<div class='d-flex image-box'>";
+        echo "<a href='#'><img class='img-fluid' width='667px' height='500px' src='$imagePath'></a>";
+        echo "</a></div>";
+        echo "</div>";
+        echo "</div>";
+        echo "<div class='row'>";
+        echo "<div class='col d-flex justify-content-center'>";
+        echo "<div class='text-center'>";
+        echo "<p class='text-center'>Date : $date";
+        echo "<h4 class='text-center'>$title</h4>";
+        echo "<p style='text-align: justify;width: 90%;max-width: 768px;'>$text</p>";
+        echo "<a class='btn' role='button' data-bss-hover-animate='pulse' href='$link' style='background: var(--bs-secondary);color: var(--bs-body-bg);text-shadow: 0px 0px 8px var(--bs-black);'>Réserver votre place</a>";
+        echo "</div>";
+        echo "</div>";
+        echo "</div>";
+        echo "<hr>";
+        echo "</div>";
+        echo "</section>";
+    }
+} else {
+    echo "Aucune image trouvée.";
+}
+
+$conn->close();
+?>
